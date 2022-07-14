f383ef5ab468b4db6ff03133a5229d6c9fddc38b
Julia Garant <47201384+juliagarant@users.noreply.github.com>
2022-07-14 15:49:15 -0400

    Dec 10, 2020

---
+++ comp2067/lab1.py
@@ -0,0 +1,9 @@
+"""
+Lab 1 for Programming for Beginners
+Julia Garant
+Jan 15 2020
+"""
+print("**aaaaaaaaaaaaaaaaaaaaaaaaa**")
+print("**aaabbbbbbbbbbbbbbbbbbbaaa**")
+print("**aaabbbbbbbbbbbbbbbbbbbaaa**")
+print("**aaaaaaaaaaaaaaaaaaaaaaaaa**")

84ffc999655e7d02682a45fed75a426036b87773
Julia Garant <47201384+juliagarant@users.noreply.github.com>
2022-07-14 15:50:22 -0400

    Dec 10, 2020

---
+++ comp2067/lab2.py
@@ -0,0 +1,8 @@
+"""
+Lab 2 for Programming for Beginners
+My First Flowchart
+Julia Garant
+Jan 23 2020
+"""
+favNumber = input("What's your favourite number?")
+print(favNumber + " is a great number!")
